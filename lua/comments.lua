d94a3ee905cade971dd687926f631bddd16137aa
OliverHeffernan <oliverheffernan@icloud.com>
2025-01-08 09:51:29 +1300

    yet another commit

--- lua/comments.lua
+++ lua/comments.lua
@@ -2,7 +2,7 @@ local function comment_based_on_context()
 	local save_pos = vim.fn.getpos(".")
 	local syntax = vim.fn.synstack(vim.fn.line("."), vim.fn.col("."))
 	local syntax_name = (#syntax == 0) and "" or vim.fn.synIDattr(syntax[#syntax], "name")
-	print("Current syntax: " .. syntax_name)
+	vim.notify("Current syntax: " .. syntax_name)
 end
 
 vim.api.nvim_set_keymap("n", "<C-/>", [[<Cmd>lua require('comments').lua comment_based_on_context()<CR>]], { noremap = true, silent = true })
