72810fc83bc013ba0f00ae8fd32c9f5c11fb4473
OliverHeffernan <oliverheffernan@icloud.com>
2025-01-08 10:11:14 +1300

    added functionality for lua files

--- lua/comments.lua
+++ lua/comments.lua
@@ -1,9 +1,22 @@
 local function comment_based_on_context()
-	vim.notify("Function Triggered!")
+	--vim.notify("Function Triggered!")
 	local save_pos = vim.fn.getpos(".")
 	local syntax = vim.fn.synstack(vim.fn.line("."), vim.fn.col("."))
 	local syntax_name = (#syntax == 0) and "" or vim.fn.synIDattr(syntax[#syntax], "name")
-	vim.notify("Current syntax: " .. syntax_name)
+	local filetype = vim.bo.filetype
+	
+	local filetype_action = {
+		lua = function()
+			vim.cmd("normal! I--")
+			vim.fn.setpos('.', save_pos)
+			vim.cmd("normal! <Esc>"
+		end
+	}
+
+	if filetype_action[filetype] then
+		filetype_action[filetype]()
+	end
+	--vim.notify("Current syntax: " .. syntax_name)
 end
 
 vim.api.nvim_set_keymap("n", "<C-_>", [[<Cmd>lua require('comments').comment_based_on_context()<CR>]], { noremap = true, silent = true })
